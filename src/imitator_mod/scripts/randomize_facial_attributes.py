1a0724d94bb58ce9d3da2a17901efc2fefffba9a
Federico Bertola <federico.bertola88@gmail.com>
2020-08-16 22:47:53 +0200

    Update randomize_facial_attributes.py
    Print and extract facial attributes

--- src/imitator_mod/scripts/randomize_facial_attributes.py
+++ src/imitator_mod/scripts/randomize_facial_attributes.py
@@ -4,7 +4,10 @@ import json
 import socket
 import socketserver
 import struct
+import sys
+import traceback
 
+import services
 from protocolbuffers import PersistenceBlobs_pb2
 from server_commands.argument_helpers import get_optional_target, OptionalSimInfoParam
 import threading
@@ -30,7 +33,7 @@ def write_objects(sock, objects):
 class Server(socketserver.ThreadingTCPServer):
     def __init__(self, server_address, callback=None, bind_and_activate=True):
         if not callable(callback):
-            callback = lambda x: [randomize_facial_attributes()]
+            callback = lambda x: [randomize_facial_attributes(o) for o in x]
 
         class IPCHandler(socketserver.BaseRequestHandler):
             def handle(self):
@@ -48,25 +51,36 @@ class Server(socketserver.ThreadingTCPServer):
         )
 
 
-def randomize_facial_attributes():
-    sim_info = get_optional_target(
-        None, target_type=OptionalSimInfoParam, _connection=None
-    )
-    if sim_info is None:
-        return {"error": "SimInfo is None"}
+def randomize_facial_attributes(params):
+    try:
+        first_name = params["first_name"]
+        last_name = params["last_name"]
+        sim_id = 0
+        info = services.sim_info_manager().get_sim_info_by_name(first_name, last_name)
+        if info is not None:
+            sim_id = info.id
 
-    facial_attributes = PersistenceBlobs_pb2.BlobSimFacialCustomizationData()
-    facial_attributes.MergeFromString(sim_info.facial_attributes)
+        sim_info = get_optional_target(
+            OptionalSimInfoParam(str(sim_id)), target_type=OptionalSimInfoParam, _connection=None
+        )
+        if sim_info is None:
+            return {"error": "SimInfo is None"}
 
-    payload = {"sculpts": str(facial_attributes.sculpts)}
+        facial_attributes = PersistenceBlobs_pb2.BlobSimFacialCustomizationData()
+        facial_attributes.MergeFromString(sim_info.facial_attributes)
 
-    for modifier in itertools.chain(
-        facial_attributes.face_modifiers, facial_attributes.body_modifiers
-    ):
-        payload[str(modifier.key)] = str(modifier.value)
+        payload = {"sculpts": str(sim_info.facial_attributes)}
 
-    sim_info.facial_attributes = facial_attributes.SerializeToString()
-    return payload
+        for modifier in itertools.chain(
+            facial_attributes.face_modifiers, facial_attributes.body_modifiers
+        ):
+            payload[str(modifier.key)] = str(modifier.amount)
+
+        sim_info.facial_attributes = facial_attributes.SerializeToString()
+        return payload
+    except Exception as e:
+        exc_info = sys.exc_info()
+        return {"exception": ''.join(traceback.format_exception(*exc_info))}
 
 
 server = Server(("127.0.0.1", 9000))
