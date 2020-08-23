dfff312171d298bef50a7e470bc5487247cb7358
Federico Bertola <federico.bertola88@gmail.com>
2020-08-23 03:08:16 +0200

    Implemented outfits extraction

--- src/imitator_mod/scripts/randomize_facial_attributes.py
+++ src/imitator_mod/scripts/randomize_facial_attributes.py
@@ -1,6 +1,5 @@
 import atexit
-import itertools
-import json
+import pickle
 import socket
 import socketserver
 import struct
@@ -8,7 +7,7 @@ import sys
 import traceback
 
 import services
-from protocolbuffers import PersistenceBlobs_pb2
+from protocolbuffers import PersistenceBlobs_pb2, Outfits_pb2
 from server_commands.argument_helpers import get_optional_target, OptionalSimInfoParam
 import threading
 
@@ -21,13 +20,18 @@ def read_objects(sock):
     data = sock.recv(size - 4)
     if len(data) == 0:
         raise ConnectionError()
-    return json.loads(data)
+    return pickle.loads(data)
 
 
 def write_objects(sock, objects):
-    data = json.dumps(objects)
+    try:
+        data = pickle.dumps(objects)
+    except Exception as e:
+        exc_info = sys.exc_info()
+        data = {"exception": ''.join(traceback.format_exception(*exc_info))}
+
     sock.sendall(struct.pack("!i", len(data) + 4))
-    sock.sendall(data.encode())
+    sock.sendall(data)
 
 
 class Server(socketserver.ThreadingTCPServer):
@@ -55,29 +59,45 @@ def randomize_facial_attributes(params):
     try:
         first_name = params["first_name"]
         last_name = params["last_name"]
-        sim_id = 0
         info = services.sim_info_manager().get_sim_info_by_name(first_name, last_name)
-        if info is not None:
-            sim_id = info.id
+
+        if info is None:
+            return {"error": f"cannot find Sim {first_name} {last_name}"}
 
         sim_info = get_optional_target(
-            OptionalSimInfoParam(str(sim_id)), target_type=OptionalSimInfoParam, _connection=None
+            OptionalSimInfoParam(str(info.id)), target_type=OptionalSimInfoParam, _connection=None
         )
+
         if sim_info is None:
             return {"error": "SimInfo is None"}
 
         facial_attributes = PersistenceBlobs_pb2.BlobSimFacialCustomizationData()
         facial_attributes.MergeFromString(sim_info.facial_attributes)
 
-        payload = {"sculpts": str(sim_info.facial_attributes)}
+        sim_proto = services.get_persistence_service().get_sim_proto_buff(sim_info.sim_id)
 
-        for modifier in itertools.chain(
-            facial_attributes.face_modifiers, facial_attributes.body_modifiers
-        ):
-            payload[str(modifier.key)] = str(modifier.amount)
+        if sim_proto is None:
+            return {"error": "Cannot get SimInfo ProtoBuf object"}
+
+        current_outfit = list(sim_proto.outfits.outfits)[sim_proto.current_outfit_index]
+
+        payload = {
+            "type": str(type(sim_info)),
+            "face_modifiers": {},
+            "body_modifiers": {},
+            "sculpts": list(facial_attributes.sculpts),
+            "outfit": current_outfit
+        }
+
+        for modifier in facial_attributes.face_modifiers:
+            payload["face_modifiers"][str(modifier.key)] = str(modifier.amount)
+
+        for modifier in facial_attributes.body_modifiers:
+            payload["body_modifiers"][str(modifier.key)] = str(modifier.amount)
 
         sim_info.facial_attributes = facial_attributes.SerializeToString()
         return payload
+
     except Exception as e:
         exc_info = sys.exc_info()
         return {"exception": ''.join(traceback.format_exception(*exc_info))}
