1b3843a208f15392a0167d93691ea5d28ba332f5
Federico Bertola <8020907+fbertola@users.noreply.github.com>
2020-08-26 01:11:27 +0200

    Read JSONs from archive if inside TS4

--- src/imitator_mod/scripts/data.py
+++ src/imitator_mod/scripts/data.py
@@ -1,15 +1,30 @@
 import json
+import zipfile
 
 from pathlib import Path
 
 json_dir = Path(__file__).parent
 
-# FIXME: read from zipfile if inside TS4
-with open(json_dir / "face_modifiers.json") as fp:
-    face_modifiers = json.load(fp)
+if json_dir.is_file():
+    # Read from zip
+    with zipfile.ZipFile(json_dir) as z:
+        with z.open("Scripts/face_modifiers.json") as fp:
+            data = fp.read()
+            face_modifiers = json.loads(data.decode())
 
-with open(json_dir / "facial_sculpts.json") as fp:
-    facial_sculpts = json.load(fp)
+        with z.open("Scripts/facial_sculpts.json") as fp:
+            data = fp.read()
+            facial_sculpts = json.loads(data.decode())
 
-with open(json_dir / "facial_casps.json") as fp:
-    facial_casps = json.load(fp)
+        with z.open("Scripts/facial_casps.json") as fp:
+            data = fp.read()
+            facial_casps = json.loads(data.decode())
+else:
+    with open(json_dir / "face_modifiers.json") as fp:
+        face_modifiers = json.load(fp)
+
+    with open(json_dir / "facial_sculpts.json") as fp:
+        facial_sculpts = json.load(fp)
+
+    with open(json_dir / "facial_casps.json") as fp:
+        facial_casps = json.load(fp)
